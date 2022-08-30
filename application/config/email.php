4b14753f8f96a6c55e0ddc6b1d607ab8dc255541
Adam PM <adam.pm77@gmail.com>
2022-08-30 15:12:01 +0700

    update

--- application/config/email.php
+++ application/config/email.php
@@ -1,8 +1,7 @@
 <?php
-$config['protocol']  = 'smtp';
-$config['smtp_host'] = 'mail.poincoin.k-rbu.com';
+$config['protocol']     = 'smtp';
+$config['smtp_host']    = 'mail.poincoin.k-rbu.com';
 $config['smtp_port']    = '587';
-// $config['smtp_port']    = '2079';
 $config['smtp_timeout'] = '30';
 $config['smtp_user']    = 'noreply@poincoin.k-rbu.com';
 $config['smtp_pass']    = 'P#mSVvJup(]t';
