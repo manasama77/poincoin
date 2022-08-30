43e585f3d3478bdb64a67eeb2106c4acc77dab69
Adam PM <adam.pm77@gmail.com>
2022-08-31 05:47:01 +0700

    update email pref

--- application/config/email.php
+++ application/config/email.php
@@ -4,7 +4,7 @@ $config['smtp_crypto'] = 'ssl';
 $config['smtp_host']   = 'mail.k-rbu.com';
 $config['smtp_port']   = '465';
 $config['smtp_user']   = 'noreply@k-rbu.com';
-$config['smtp_pass']   = 'P#mSVvJup(]t';
+$config['smtp_pass']   = 'F4=H0j)T]@eq';
 $config['charset']     = 'utf-8';
 $config['newline']     = "\r\n";
 $config['mailtype']    = 'html';
